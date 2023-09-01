89cf8e25992ecaef7fa98a838f985ce22965d797
bokkle <mitch.r_@hotmail.com>
2023-09-01 01:57:51 -0400

    complete section

--- assignments.js
+++ assignments.js
@@ -158,8 +158,182 @@ const calcTip = (bill) => {
 };
 
 const bills = [125, 555, 44];
-console.log(bills)
+console.log(bills);
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 console.log(tips);
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(totals);
+
+// ASSIGNMENT 7
+console.log("Assignment 7:");
+
+const myCountry = {
+  country: "Canada",
+  capital: "Ottawa",
+  language: "English",
+  population: 40,
+  neighbour: ["USA"],
+  describe: function () {
+    return `${this.country} has ${this.population} million ${this.language} speaking people, ${this.neighbour.length} neighbouring country (${this.neighbour}) and it's capital is ${this.capital}`;
+  },
+  checkIsIsland: function () {
+    this.isIsland = this.neighbour.length === 0 ? true : false;
+    return this.isIsland;
+  },
+};
+
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language} speaking people, ${myCountry.neighbour.length} neighbouring country (${myCountry.neighbour}) and it's capital is ${myCountry.capital}`
+);
+
+const boostPop = (myCountry.population += 2);
+console.log(boostPop);
+const lowerPop = (myCountry["population"] -= 2);
+console.log(lowerPop);
+
+//ASSIGNMENT 8
+console.log("Assignment 8:");
+console.log(myCountry.describe());
+console.log(myCountry.checkIsIsland());
+console.log(myCountry);
+
+//CODING CHALLENGE 3
+console.log("Coding Challenge 3:");
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+// implement the calculations! Remember: BMI = mass / height ** 2 = mass
+// / (height * height) (mass in kg and height in meter)
+// Your tasks:
+// 1. For each of them, create an object with properties for their full name, mass, and
+// height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+// method on both objects). Store the BMI value to a property, and also return it
+// from the method
+// 3. Log to the console who has the higher BMI, together with the full name and the
+// respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+// Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+// tall.
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+mark.calcBMI();
+john.calcBMI();
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`
+  );
+} else {
+  console.log("They have the same BMI.");
+}
+
+//ASSIGNMENT 9
+console.log("Assignment 9:");
+
+for (let i = 1; i <= 50; i++) {
+  console.log(`Voter number ${i} is currently voting`);
+}
+
+//ASSIGNMENT 10
+console.log("Assignment 10:");
+// const populations = [40, 355, 80, 12];
+// function percentageOfWorld(population) {
+//     return (population / 7900) * 100;
+//   }
+const percentages2 = [];
+for (let i = 0; i < populations.length; i++) {
+  let percentage = percentageOfWorld(populations[i]);
+  percentages2.push(`${percentage.toFixed(2)}%`);
+}
+console.log(
+  "Population (millions):",
+  populations,
+  "% of world pop:",
+  percentages2
+);
+
+//ASSIGNMENT 11
+console.log("Assignment 11:");
+
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  console.log("---Neighbour(s)---");
+  for (let j = 0; j < listOfNeighbours[i].length; j++) {
+    console.log(listOfNeighbours[i][j]);
+  }
+}
+
+//CHALLENGE 4
+console.log("Coding Challenge 4:");
+
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+// 1. Create an array 'bills' containing all 10 test bill values
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+// tips and total values (bill + tip) for every bill value in the bills array. Use a for
+// loop to perform the 10 calculations!
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+// tips and totals arrays �
+// Bonus:
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+// an argument. This function calculates the average of all numbers in the given
+// array. This is a difficult challenge (we haven't done this before)! Here is how to
+// solve it:
+// 4.1. First, you will need to add up all values in the array. To do the addition,
+// start by creating a variable 'sum' that starts at 0. Then loop over the
+// array using a for loop. In each iteration, add the current value to the
+// 'sum' variable. This way, by the end of the loop, you have all values
+// added together
+// 4.2. To calculate the average, divide the sum you calculated before by the
+// length of the array (because that's the number of elements)
+// 4.3. Call the function with the 'totals' array
+
+const bills2 = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips2 = [];
+const totals2 = [];
+
+for (let i = 0; i < bills2.length; i++) {
+  tips2.push(calcTip(bills2[i]));
+  totals2.push(bills2[i] + calcTip(bills2[i]));
+}
+console.log(bills2);
+console.log("Tips:", tips2);
+console.log("totals:", totals2);
+
+const calcAvgBill = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAvgBill(totals2));
--- script.js
+++ script.js
@@ -115,4 +115,221 @@ console.log(friends.includes('bob')) // returns false
 if (friends.includes('tim')) {
     console.log('Tim is your boi')
 } 
+
+
+//OBJECTS
+//instead of
+const mitchArray = [
+  "Mitch",
+  "Riccio",
+  1996,
+  "programmer",
+  ["sani", "nedo", "benny", "bruce"],
+];
+//can use
+const mitchObject = {
+  firstName: "Mitch",
+  lastName: "Riccio",
+  birthYear: 1996,
+  job: "programmer",
+  friends: ["sani", "nedo", "benny", "bruce"],
+};
+console.log(mitchObject);
+
+//retrieve info from obj...
+
+//DOT NOTATION
+console.log(mitchObject.lastName);
+
+//BRACKET NOTATION
+console.log(mitchObject["lastName"]);
+
+const nameKey = "Name";
+console.log(mitchObject["first" + nameKey]);
+console.log(mitchObject["last" + nameKey]);
+
+//if you dont know which property to show, and you get the
+//info from a UI...
+
+// const interestedIn = prompt(
+//   "What do you want to know about Mitch? Choose between firstName, lastName, birthYear, job, and friends"
+// );
+// console.log(mitchObject[interestedIn])
+
+// if (mitchObject[interestedIn]) {
+//     console.log(mitchObject[interestedIn])
+// } else {
+//     console.log('Wrong request!')
+// }
+
+//add new properties
+
+mitchObject.location = "Canada";
+mitchObject["twitter"] = "@MitchIsBased";
+console.log(mitchObject);
+
+//mini challenge
+//'Mitch has 3 friends and his best friend is named 'benny'
+console.log(
+  `${mitchObject.firstName} has ${mitchObject.friends.length} friends and his best friend is named ${mitchObject.friends[2]}`
+);
+//We've hired a new programmer, his name is mitch riccio, he was born in 1996 and he's brought his friend bruce with him today
+console.log(
+  `We've hired a new ${mitchObject.job}, his name is ${mitchObject.firstName} ${mitchObject.lastName}, he was born in ${mitchObject.birthYear} and he brought his friend ${mitchObject.friends[3]} with him today`
+);
+
+const bokkle = {
+  name: "bokkle",
+  class: "bruiser",
+  strength: 300,
+  con: 200,
+  pocket: "Luna",
+};
+
+const _4PF = `${bokkle.name} is the best ${bokkle.class} on the server. He runs ${bokkle.strength} strength and ${bokkle.con} con, all of you should design your build after his. Also, he is unkillable when his pocket ${bokkle.pocket} is with him.`
+console.log(_4PF)
+
+const bokkleInfo = prompt('What would you like to know about bokkle?')
+
+if (bokkle[bokkleInfo]) {
+    console.log(bokkle[bokkleInfo])
+} else {
+    console.log('wrong input')
+}
+
+
+const bokkle = {
+  name: "bokkle",
+  class: "bruiser",
+  strength: 300,
+  birthYear: 1996,
+  con: 200,
+  pocket: "Luna",
+  hasBFL: true,
+  friends: ["yungin", "TARDY", "leilow"],
+
+  // calcAge: function (birthYear) {
+  //     return 2023 - birthYear
+  // }
+
+  // calcAge: function () {
+  //     console.log(this)
+  //     return 2023 - this.birthYear
+  // }
+
+  calcAge: function () {
+    this.age = 2023 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.name} is a ${this.calcAge()} year old ${this.class}. He has ${
+      this.hasBFL ? "a" : "no"
+    } Battlefield Legend title.`;
+  },
+};
+
+console.log(bokkle.calcAge(1991));
+//   console.log(bokkle['calcAge'](1996))
+console.log(bokkle.age);
+console.log(bokkle.getSummary());
+
+//MINI CHALLENGE
+//getSummary() summarize the data about bokkle
+//'bokkle is a 27 year old bruiser. He has a/no battlefield legend title'
+
+// LOOPS   
+console.log('Bench repitition 1')
+console.log('Bench repitition 2')
+console.log('Bench repitition 3')
+console.log('Bench repitition 4')
+console.log('Bench repitition 5')
+console.log('Bench repitition 6')
+console.log('Bench repitition 7')
+console.log('Bench repitition 8')
+console.log('Bench repitition 9')
+console.log('Bench repitition 10')
+
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Bench repitition ${rep}`)
+}
+
+const bokkle = [
+  "bokkle",
+  "bruiser",
+  300,
+  200,
+  "BFL",
+  ["yungin", "TARDY", "leilow"],
+];
+
+const types = [];
+
+// for (let i = 0; i < bokkle.length; i++) {
+//     console.log(bokkle[i], typeof bokkle[i])
+//     types.push(typeof bokkle[i])
+// }
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  let age = 2023 - years[i];
+  ages.push(age);
+}
+console.log(ages);
+
+//continue and break
+console.log("ONLY STRINGS");
+for (let i = 0; i < bokkle.length; i++) {
+  if (typeof bokkle[i] !== "string") continue; //only prints the strings
+  console.log(bokkle[i], typeof bokkle[i]);
+}
+
+console.log("BREAK @ NUMBER");
+for (let i = 0; i < bokkle.length; i++) {
+  if (typeof bokkle[i] === "number") break; //logs everything up until the first number, at which point, the loop is exited
+  console.log(bokkle[i], typeof bokkle[i]);
+}
+
+//loop backwards thru array
+const bokkle = [
+  "bokkle",
+  "bruiser",
+  300,
+  200,
+  "BFL",
+  ["yungin", "TARDY", "leilow"],
+];
+
+for (let i = bokkle.length - 1; i >= 0; i--) {
+  console.log(i, bokkle[i]);
+}
+
+//LOOP WITHIN A LOOP
+//log 3 sets of 5 reps each
+
+for (let set = 1; set <= 3; set++) {
+    console.log(`---SET ${set}---`)
+    for (let rep = 1; rep <= 5; rep++) {
+        console.log(`Rep ${rep}`)
+    }
+}
+
+// WHILE LOOP
+let rep = 1 
+while (rep <= 10) {
+    console.log(`WHILE: Repitition ${rep}`)
+    rep++
+}
+
+let dice = Math.floor(Math.random() * 7)
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`)
+    dice = Math.floor(Math.random() * 7)
+    if (dice === 6) console.log('Winner')
+}
 */
